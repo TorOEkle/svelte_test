ad255f68a6fafc2c1ca344c9d930858e4747a755
Tor Odin <tor-odin.ekle@capgemini.com>
2023-02-08 12:17:14 +0100

    Updated with test page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,7 +18,7 @@
 			</picture>
 		</span>
 
-		to your new<br />SvelteKit app
+		To your new<br />SvelteKit app
 	</h1>
 
 	<h2>
---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,22 @@
+<svelte:head>
+	<title>Test</title>
+	<meta name="test" content="test to add new pages" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Test</h1>
+
+	<p>
+		This is a test page for Tor Odin Ekle to learn Svelte. For more projects check my <a href="https://github.com/TorOEkle">GitHub</a>. 
+		This page is built with SvelteKit. 
+	</p>
+
+	<p>
+		Tor Odin is an Economist with mechanical background from manufacturing and oil industry that has went into tech and want to learn more. 
+		Tools that I focus to learn more about now is Python, Streamlit, SQL and Svelte. 
+	</p>
+</div>
+
+<svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+	<circle cx="5" cy="5" r="5" />
+  </svg>
