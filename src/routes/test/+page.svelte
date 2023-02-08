85d4da0697ec6919eda089c0123bcb8c7363fc98
Tor Odin <tor-odin.ekle@capgemini.com>
2023-02-08 12:49:46 +0100

    Updated with form interact with SVG circle

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -16,7 +16,42 @@
 		Tools that I focus to learn more about now is Python, Streamlit, SQL and Svelte. 
 	</p>
 </div>
+<script lang="ts">
+	import { user } from "./store.js";
+	function onSubmit(e) {
+	  const formData = new FormData(e.target);
+  
+	  const data = {};
+	  for (let field of formData) {
+		const [key, value] = field;
+		data[key] = value;
+	  }
+	  console.log(data)
+	}
+  </script>
+
+<h1>Design Circle</h1>
+<form class="content">
+  <label>CX</label>
+  <input type="text" bind:value={$user.cx} />
+  <label>CY</label>
+  <input type="text" bind:value={$user.cy} />
+  <label>Radius</label>
+  <input type="text" bind:value={$user.radius} />
+  <label>Stroke[HEX]</label>
+  <input type="text" bind:value={$user.stroke} />
+  <label>Fill[HEX]</label>
+  <input type="text" bind:value={$user.fill} />
+</form>
+ 
+ <style>
+  .content {
+    display: grid;
+    grid-template-columns: 20% 80%;
+    grid-column-gap: 10px;
+  }
+ </style>
 
 <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-	<circle cx="5" cy="5" r="5" />
+	<circle cx={$user.cx} cy={$user.cy} r={$user.radius} stroke = {$user.stroke} fill = {$user.fill} />
   </svg>
